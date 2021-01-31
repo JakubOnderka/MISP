548345b6a5b9132a76f60e41fa8f752c8451eb5b
Jakub Onderka <jakub.onderka@gmail.com>
2021-01-31 13:43:38 +0100

    new: [log] Show full change in popup

--- app/Controller/Component/ACLComponent.php
+++ app/Controller/Component/ACLComponent.php
@@ -370,6 +370,7 @@ class ACLComponent extends Component
             ),
       'auditLogs' => [
           'admin_index' => ['perm_audit'],
+          'fullChange' => ['perm_audit'],
           'eventIndex' => ['*'],
           'returnDates' => ['*'],
       ],
